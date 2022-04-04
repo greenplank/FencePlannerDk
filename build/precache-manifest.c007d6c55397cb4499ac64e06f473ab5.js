eaf65549ea98522c5f041e8dea60459a2891d645
greenplank <info@greenplank.eu>
2022-04-04 10:35:07 +0500

    newDKValue

---
+++ build/precache-manifest.c007d6c55397cb4499ac64e06f473ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648238223754931bedcb6e05d8267d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7a609d59a3966a5ee9",
+    "url": "/static/css/main.7eeff2ba.chunk.css"
+  },
+  {
+    "revision": "90bdde2d0e84a75e41a3",
+    "url": "/static/js/2.fbac3181.chunk.js"
+  },
+  {
+    "revision": "e1a3eec440a54a70d5dda1771ab9d24e",
+    "url": "/static/js/2.fbac3181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7a609d59a3966a5ee9",
+    "url": "/static/js/main.7c5cadcc.chunk.js"
+  },
+  {
+    "revision": "f1c6c453956d6fc806f1",
+    "url": "/static/js/runtime-main.57cc7a46.js"
+  }
+]);
